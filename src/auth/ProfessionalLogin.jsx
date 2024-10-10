712ae55a9bb9847e284f6d0241278d3a7df87270
vikramdev772 <vikramdev772@gmail.com>
2024-10-10 21:29:00 +0530

    ProfessionalLogin

--- src/auth/ProfessionalLogin.jsx
+++ src/auth/ProfessionalLogin.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import { motion } from 'framer-motion';
 import { Link, useNavigate } from 'react-router-dom';
-import LoginModal from './LoginModal'
 import PasswordModal from './PasswordModal';
 import OtpModal from './OtpModal';
 import RecoverPasswordModal from './RecoverPasswordModal';
